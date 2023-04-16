d4a9da767f61eb61a6a9603cbe63c07d31259897
Maulana Bayu Risma Santoso Sari <maulana.bayu@ui.ac.id>
2023-04-16 17:31:32 +0700

    simple API project

---
+++ api/views.py
@@ -0,0 +1,28 @@
+from rest_framework import generics
+from rest_framework import permissions
+from api import serializers
+from api.models import Post
+from api.permissions import IsOwnerOrReadOnly
+from django.contrib.auth.models import User
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
